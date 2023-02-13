139d52f150801f69763b56c1370686780d1eb3aa
cheetamaru <vitalvat@gmail.com>
2023-02-13 20:36:02 +0200

    feat: add components and use props

---
+++ src/components/Card.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let pokemon = "";
+</script>
+
+<div>Name: {pokemon}</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<script>
+    import Card from "../components/Card.svelte"
+
+    let content = ['Pikachu', "Bilbasaur", "Greninja"]
+</script>
+
 <div>
     <h1>Svelte Pokemon Pet Project</h1>
     <main>
-        List of contents
+        <h2>List of contents</h2>
+        {#each content as pokemon}
+            <Card {pokemon} ></Card>
+        {/each}
     </main>
 </div>
