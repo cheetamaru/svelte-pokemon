2551de408886251b265279671879fc967dc412e2
cheetamaru <vitalvat@gmail.com>
2023-02-14 20:57:44 +0200

    feat: try to use awat markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,19 @@
     import Card from "../components/Card.svelte"
 
     let content = ['Pikachu', "Bilbasaur", "Greninja"]
+
+    const getDitto = async () => {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        const dittoObj = await res.json()
+
+        if (res.ok) {
+			return dittoObj;
+		}
+        
+		throw new Error(dittoObj);
+    }
+
+    const ditto = getDitto()
 </script>
 
 <div>
@@ -11,5 +24,14 @@
         {#each content as pokemon}
             <Card {pokemon} ></Card>
         {/each}
+        <div>Name: Ditto</div>
+        {#await ditto}
+            <div>...loading</div>
+        {:then info} 
+            <div>Picture:</div>
+            <img src="{info.sprites.front_default}" alt="Ditto">
+        {:catch error}
+            <div>Error: {error}</div>
+        {/await}
     </main>
 </div>
