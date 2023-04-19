8ee44b8edbea66beabb99d0f137d175640ead67d
cheetamaru <vitalvat@gmail.com>
2023-04-19 21:58:46 +0300

    feat: add not found fallback to pokemon info

--- src/components/PokemonInfo.svelte
+++ src/components/PokemonInfo.svelte
@@ -17,13 +17,15 @@
 </script>
 
 <div class="pokemon-info">
-    <div class="pokemon-info__title">{infoTitle}</div>
+    <div class="pokemon-info__title">{infoTitle}</div>    
     {#if src}
         <img class="pokemon-info__img" {src} {alt}/>
     {:else}
-        <div class="pokemon-info__not-found">😢😭😨<br>Image<br>Not<br>found</div>
+        <div class="pokemon-info__not-found">
+            <img class="pokemon-info__not-found-img" src="/substitute_sprite.png" {alt}/>
+            <div class="pokemon-info__not-found-subtitle">Image not found</div>
+        </div>
     {/if}
-    
 </div>
 
 <style>
@@ -43,7 +45,20 @@
 
     .pokemon-info__not-found {
         height: 96px;
-        width: 96px;
         text-align: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .pokemon-info__not-found-img {
+        height: 60%;
+    }
+
+    .pokemon-info__not-found-subtitle {
+        font-weight: 100;
+        font-size: small;
+        color: gray;
     }
 </style>
